b0af4dcb3afba91f4c0fb2bc18d8e319bf98478f
Gris87 <Gris87@yandex.ru>
2014-03-30 23:02:40 +0400

    Demo

---
+++ Assets/Scripts/FPSCounter/Demo/FPS_DemoScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPS_DemoScript : MonoBehaviour
+{
+	private string[] qualities;
+
+	// Use this for initialization
+	void Start ()
+	{
+		qualities=QualitySettings.names;
+
+		FPSCounter.isOn=true;
+	}
+
+	void OnGUI()
+	{
+		if (GUI.Button(new Rect(Screen.width*0.01f, Screen.height*0.9f, Screen.width*0.1f, Screen.height*0.05f), FPSCounter.isOn ? "ON" : "OFF"))
+		{
+			FPSCounter.isOn=!FPSCounter.isOn;
+		}
+
+		for (int i=0; i<qualities.Length; ++i)
+		{
+			if (GUI.Button(new Rect(Screen.width*0.01f, Screen.height*(0.05f+0.06f*i), Screen.width*0.1f, Screen.height*0.05f), i==QualitySettings.GetQualityLevel() ? (">>> "+qualities[i]+" <<<") : qualities[i]))
+			{
+				QualitySettings.SetQualityLevel(i);
+			}
+		}
+	}
+}
